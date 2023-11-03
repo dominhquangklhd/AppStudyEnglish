75c5b3bea4cf854daee3682264c65749df6a39ef
2uangthor <minhquangkl04@gmail.com>
2023-11-03 10:53:49 +0700

    add mainCommandLine

--- src/main/java/app/DB_Connection/DatabaseConnection.java
+++ src/main/java/app/DB_Connection/DatabaseConnection.java
@@ -47,9 +47,13 @@ public class DatabaseConnection {
             username = "root";
             password = "Boquoctrung10012004";*/
 
-            url = "jdbc:mysql://localhost:3306/appEnglish";
+            /*url = "jdbc:mysql://localhost:3306/appEnglish";
             username = "root";
-            password = "Minhquanadc@1";
+            password = "Minhquanadc@1";*/
+
+            url = "jdbc:mysql://localhost:3306/dict_database";
+            username = "root";
+            password = "Q25012004kl#";
 
             connection = DriverManager.getConnection(url, username, password);
 
